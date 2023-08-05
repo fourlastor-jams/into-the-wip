046d580072e6c72660a45e5af7d1a24035396655
Daniele Conti <fourlastor@users.noreply.github.com>
2023-08-05 17:48:13 +0100

    Move map tiles in assets

--- assets/maps/Tiles.tsx
+++ assets/maps/Tiles.tsx
@@ -2,6 +2,6 @@
 <tileset version="1.10" tiledversion="1.10.1" name="Tiles" tilewidth="62" tileheight="31" tilecount="1" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="2">
-  <image width="62" height="31" source="../../raw assets/map-tile.png"/>
+  <image width="62" height="31" source="../images/included/map-tile.png"/>
  </tile>
 </tileset>
