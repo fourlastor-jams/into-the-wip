48eb82ddc65bde32c83a8862052bacac735c0b70
SheerSt <8891626+SheerSt@users.noreply.github.com>
2023-08-05 15:47:10 -0500

    Add basic tiles; try demo map; move .tsx files

--- assets/maps/Tiles.tsx
+++
@@ -1,7 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.1" name="Tiles" tilewidth="62" tileheight="31" tilecount="1" columns="0">
- <grid orientation="orthogonal" width="1" height="1"/>
- <tile id="2">
-  <image width="62" height="31" source="../images/included/map-tile.png"/>
- </tile>
-</tileset>
