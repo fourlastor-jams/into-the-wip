36696add2096c29d2a82ff1dc6fd01f3737ed4b3
Daniele Conti <fourlastor@users.noreply.github.com>
2023-08-06 10:01:39 +0100

    Use lowercase everywhere

--- assets/images/included/Monsters.tsx
+++ assets/images/included/Monsters.tsx
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.2" name="Monsters" tilewidth="32" tileheight="64" tilecount="4" columns="2" objectalignment="bottomleft">
  <tileoffset x="16" y="0"/>
- <image source="Monsters.png" width="64" height="128"/>
+ <image source="monsters.png" width="64" height="128"/>
  <tile id="0">
   <animation>
    <frame tileid="0" duration="250"/>
