0b977793177726f6d55a27f459905fd1c6ccbba4
Daniele Conti <fourlastor@users.noreply.github.com>
2023-08-12 12:44:41 +0100

    Add enemy and walkable properties to tiles

--- assets/images/included/Monsters.tsx
+++ assets/images/included/Monsters.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="Monsters" tilewidth="32" tileheight="64" tilecount="4" columns="2" objectalignment="bottomleft">
+<tileset version="1.10" tiledversion="1.10.1" name="Monsters" tilewidth="32" tileheight="64" tilecount="4" columns="2" objectalignment="bottomleft">
  <tileoffset x="16" y="0"/>
  <image source="monsters.png" width="64" height="128"/>
  <tile id="0">
@@ -13,5 +13,15 @@
    <frame tileid="1" duration="250"/>
    <frame tileid="3" duration="250"/>
   </animation>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="enemy" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="enemy" type="bool" value="true"/>
+  </properties>
  </tile>
 </tileset>
