269d70ca2827e4bbf5bfe8c0fad17616278d5d06
Daniele Conti <fourlastor@users.noreply.github.com>
2023-08-13 10:14:01 +0100

    Use odd-q system

--- core/src/main/java/io/github/fourlastor/game/coordinates/Hex.java
+++ core/src/main/java/io/github/fourlastor/game/coordinates/Hex.java
@@ -4,7 +4,7 @@ import com.badlogic.gdx.math.GridPoint2;
 import com.badlogic.gdx.math.GridPoint3;
 
 /**
- * Hex coordinates, in offset (even-q) and cube systems.
+ * Hex coordinates, in offset (odd-q) and cube systems.
  * See <a href="https://www.redblobgames.com/grids/hexagons/#coordinates">original blog post</a>.
  * Cube is useful for pathfinding, offset is what we get from tiled, and it's useful for the on-screen coordinates.
  */
@@ -23,40 +23,22 @@ public class Hex {
         this.offset = toOffset(cube);
     }
 
-    /** x coordinate, in offset. */
-    public int x() {
-        return offset.x;
-    }
-
-    /** y coordinate, in offset. */
-    public int y() {
-        return offset.y;
-    }
-
-    /** q coordinate, in cube. */
-    public int q() {
-        return cube.x;
-    }
-
-    /** r coordinate, in cube. */
-    public int r() {
-        return cube.y;
-    }
-
-    /** s coordinate, in cube. */
-    public int s() {
-        return cube.z;
+    @Override
+    public String toString() {
+        return "Hex{" + "offset=" + offset + ", cube=" + cube + '}';
     }
 
     public static GridPoint3 toCube(GridPoint2 offset) {
         int q = offset.x;
-        int r = offset.y - (offset.x + (Math.abs(offset.x % 2))) / 2;
-        return new GridPoint3(q, r, -q - r);
+        int r = offset.y - (offset.x - (Math.abs(offset.x % 2))) / 2;
+        GridPoint3 gridPoint3 = new GridPoint3(q, r, -q - r);
+        System.out.println("Convert " + offset + " to " + gridPoint3);
+        return gridPoint3;
     }
 
     public static GridPoint2 toOffset(GridPoint3 cube) {
         int x = cube.x;
-        int y = cube.y + (cube.x + (Math.abs(cube.x % 2))) / 2;
+        int y = cube.y + (cube.x - (Math.abs(cube.x % 2))) / 2;
         return new GridPoint2(x, y);
     }
 }
