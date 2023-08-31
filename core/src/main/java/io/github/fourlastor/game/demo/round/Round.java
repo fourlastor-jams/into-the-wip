c3535218839be0ba2ec0a844934c0cc40ab4b9b3
Daniele Conti <fourlastor@users.noreply.github.com>
2023-08-31 09:44:46 +0100

    Migrate old way of checking if all units acted

--- core/src/main/java/io/github/fourlastor/game/demo/round/Round.java
+++ core/src/main/java/io/github/fourlastor/game/demo/round/Round.java
@@ -8,24 +8,20 @@ import io.github.fourlastor.game.demo.round.faction.Faction;
 import io.github.fourlastor.game.demo.state.GameState;
 import io.github.fourlastor.game.demo.state.unit.Unit;
 import io.github.fourlastor.game.ui.ActorSupport;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import javax.inject.Inject;
-import squidpony.squidmath.IRNG;
 
 public class Round extends RoundState {
 
     private final StateRouter router;
-    private final IRNG rng;
     private List<CurrentFaction> factions = null;
     private int factionCounter = 0;
 
     @Inject
-    public Round(StateRouter router, IRNG rng) {
+    public Round(StateRouter router) {
         this.router = router;
-        this.rng = rng;
     }
 
     @Override
@@ -56,7 +52,7 @@ public class Round extends RoundState {
 
     private void advanceToNextTurn(GameState state) {
         CurrentFaction currentFaction = factions.get(factionCounter);
-        if (currentFaction.alreadyDidTurn.size() >= currentFaction.units.size()) {
+        if (currentFaction.allUnitsActed()) {
             factionCounter += 1;
         }
         if (factionCounter >= factions.size()) {
@@ -70,27 +66,28 @@ public class Round extends RoundState {
         CurrentFaction currentFaction = factions.get(factionCounter);
         currentFaction.units.stream().filter(unitInTurn -> !unitInTurn.hasActed).forEach(unitInTurn -> {
             state.tileAt(unitInTurn.unit.hex).actor.setColor(Color.PINK);
-            unitInTurn.unit.group.image.addListener(new TurnListener(unitInTurn, currentFaction));
+            unitInTurn.unit.group.image.addListener(new TurnListener(unitInTurn));
         });
     }
 
     private static class CurrentFaction {
         final List<UnitInTurn> units;
-        final List<Unit> alreadyDidTurn = new ArrayList<>();
 
         private CurrentFaction(List<Unit> units) {
             this.units = units.stream().map(UnitInTurn::new).collect(Collectors.toList());
+        }
+
+        boolean allUnitsActed() {
+            return units.stream().allMatch(it -> it.hasActed);
         }
     }
 
     private class TurnListener extends ClickListener {
 
         private final UnitInTurn unit;
-        private final CurrentFaction currentFaction;
 
-        public TurnListener(UnitInTurn unit, CurrentFaction currentFaction) {
+        public TurnListener(UnitInTurn unit) {
             this.unit = unit;
-            this.currentFaction = currentFaction;
         }
 
         @Override
