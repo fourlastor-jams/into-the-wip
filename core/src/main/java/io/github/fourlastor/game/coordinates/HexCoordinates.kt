a4f6d99da63338b90f8485a941854448a52385f7
Daniele Conti <fourlastor@users.noreply.github.com>
2023-09-04 12:01:48 +0100

    Convert coordinates classes

---
+++ core/src/main/java/io/github/fourlastor/game/coordinates/HexCoordinates.kt
@@ -0,0 +1,28 @@
+package io.github.fourlastor.game.coordinates
+
+import com.badlogic.gdx.math.Vector2
+
+class HexCoordinates(private val width: Int, tileHeight: Int, hexSideLength: Int) {
+    private val height: Int
+    private val horizontalSpacing: Float
+    private val staggerSpacing: Float
+
+    init {
+        height = tileHeight - 1
+        horizontalSpacing = (width - hexSideLength) / 2f + hexSideLength - 1
+        staggerSpacing = height / 2f
+    }
+
+    fun toWorldAtOrigin(x: Int, y: Int, out: Vector2): Vector2 {
+        val stagger = if (x % 2 == 0) 0f else staggerSpacing
+        return out.set(x * horizontalSpacing, stagger + y * height)
+    }
+
+    fun toWorldAtCenter(hex: Hex, out: Vector2): Vector2 {
+        return toWorldAtCenter(hex.offset.x, hex.offset.y, out)
+    }
+
+    fun toWorldAtCenter(x: Int, y: Int, out: Vector2): Vector2 {
+        return toWorldAtOrigin(x, y, out).add(width / 2f, height / 2f)
+    }
+}
---
+++ core/src/main/java/io/github/fourlastor/game/coordinates/Packer.kt
@@ -0,0 +1,13 @@
+package io.github.fourlastor.game.coordinates
+
+import com.badlogic.gdx.math.GridPoint2
+
+object Packer {
+    fun pack(position: GridPoint2): Int {
+        return position.x shl 16 or position.y
+    }
+
+    fun unpack(value: Int, target: GridPoint2): GridPoint2 {
+        return target.set(value shr 16, value.toShort().toInt())
+    }
+}
