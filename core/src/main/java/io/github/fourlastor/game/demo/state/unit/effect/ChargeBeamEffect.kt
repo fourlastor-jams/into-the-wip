ebd696e75119428511d53a8084da82bd131188a0
SheerSt <8891626+SheerSt@users.noreply.github.com>
2023-09-08 11:56:06 -0500

    - Fix label in charge beam affect  - Use chargebeam effect Factory

--- core/src/main/java/io/github/fourlastor/game/demo/state/unit/effect/ChargeBeamEffect.kt
+++ core/src/main/java/io/github/fourlastor/game/demo/state/unit/effect/ChargeBeamEffect.kt
@@ -1,34 +1,38 @@
 package io.github.fourlastor.game.demo.state.unit.effect
 
+import com.badlogic.gdx.assets.AssetManager
 import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.scenes.scene2d.Action
 import com.badlogic.gdx.scenes.scene2d.actions.Actions
 import io.github.fourlastor.game.demo.state.unit.Mon
 import io.github.fourlastor.game.demo.state.unit.effect.Effect.Triggered
-import javax.inject.Inject
-import javax.inject.Singleton
 import com.badlogic.gdx.scenes.scene2d.ui.Label
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedFactory
+import dagger.assisted.AssistedInject
 
-@Singleton
-class ChargeBeamEffect(mon: Mon) : Triggered {
+class ChargeBeamEffect @AssistedInject constructor(
+        @Assisted mon: Mon,
+    assetManager: AssetManager
+) : Triggered {
 
-    var chargeTier: Int = 1
+    private var chargeTier: Int = 1
     val targetMon: Mon
-    val label: Label
-
+    private val label: Label
     init {
         targetMon = mon
-        val hpLabelStyle = Label.LabelStyle(assetManager.get("fonts/quan-pixel-16.fnt"), Color.BLUE)
-        val hpLabel = Label(String.valueOf(chargeTier), hpLabelStyle)
-        targetMon.group.addActor(hpLabel)
+        val labelStyle = Label.LabelStyle(assetManager.get("fonts/quan-pixel-16.fnt"), Color.BLUE)
+        label = Label(chargeTier.toString(), labelStyle)
+        label.setPosition(0f, targetMon.group.image.imageHeight + 8)
+        targetMon.group.addActor(label)
     }
 
     override fun triggerEffect(mon: Mon): Action {
         val sequence = Actions.sequence(
-            Actions.run( { 
-                println("ChargeBeamEffect::triggerEffect") 
+            Actions.run {
                 chargeTier += 1
-            } )
+                label.setText(chargeTier.toString())
+            }
         )
         sequence.setActor(mon.group.image)
         return sequence
@@ -36,5 +40,10 @@ class ChargeBeamEffect(mon: Mon) : Triggered {
 
     override fun cleanup() {
         targetMon.group.removeActor(label)
+    }
+
+    @AssistedFactory
+    interface Factory {
+        fun create(mon: Mon): ChargeBeamEffect
     }
 }
